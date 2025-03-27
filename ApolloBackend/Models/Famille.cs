ea43334ff17bafdd5c23db359f77d96d604b9d0a
MedAmineChakroun <164560880+MedAmineChakroun@users.noreply.github.com>
2025-03-27 23:03:13 +0100

    ajouter class article et famille pour bd local

---
+++ ApolloBackend/Models/Famille.cs
@@ -0,0 +1,11 @@
+﻿namespace ApolloBackend.Models
+{
+    public class Famille
+    {
+        public string FamCode { get; set; } = null!;
+
+        public string? FamIntitule { get; set; }
+
+        public int FamId { get; set; }
+    }
+}
