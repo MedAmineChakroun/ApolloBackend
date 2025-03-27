59b034360c36a2257805178780f6e2237d8e10e6
Fedi Boughnimi <fediboughnimi2@gmail.com>
2025-03-27 23:04:59 +0100

    ajouter les deux classes DocumentVente et DocumentVenteLigne

---
+++ ApolloBackend/Models/DocumentVente.cs
@@ -0,0 +1,21 @@
+﻿namespace ApolloBackend.Models
+{
+    public class DocumentVente
+    {
+        public short? DocType { get; set; }
+
+        public string? DocPiece { get; set; }
+
+        public DateTime? DocDate { get; set; }
+
+        public decimal? DocTht { get; set; }
+
+        public decimal? DocTtc { get; set; }
+
+        public string? DocTiersCode { get; set; }
+
+        public string? DocTiersIntitule { get; set; }
+
+        public int DocId { get; set; }
+    }
+}
