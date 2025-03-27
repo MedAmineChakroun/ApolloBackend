f5576514444028eadafacfedc48a669713c67d5a
MedAmineChakroun <164560880+MedAmineChakroun@users.noreply.github.com>
2025-03-27 23:31:52 +0100

    ajouter class doc vente et doc vente ligne

---
+++ ApolloBackend/Models/DocumentVenteLigne.cs
@@ -0,0 +1,21 @@
+﻿namespace ApolloBackend.Models
+{
+    public class DocumentVenteLigne
+    {
+        public string LigneDocPiece { get; set; } = null!;
+
+        public string? LigneArtCode { get; set; }
+
+        public string? LigneArtDesi { get; set; }
+
+        public decimal? LigneQte { get; set; }
+
+        public decimal? LignePu { get; set; }
+
+        public decimal? LigneHt { get; set; }
+
+        public decimal? LigneTtc { get; set; }
+
+        public int LigneId { get; set; }
+    }
+}
