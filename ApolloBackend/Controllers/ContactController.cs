053ceb732be501a317f41146c60bb59ba56ed928
MedAmineChakroun <164560880+MedAmineChakroun@users.noreply.github.com>
2025-04-09 13:16:25 +0100

    ajouter Contact Email Backend Logic (model and controller)

---
+++ ApolloBackend/Controllers/ContactController.cs
@@ -0,0 +1,50 @@
+﻿using ApolloBackend.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+
+namespace ApolloBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail([FromBody] ContactForm form)
+        {
+            try
+            {
+                const string fromPassword = "cjck cnnd htvw qhfl"; 
+                var toAdress = new MailAddress("hbnkii2@gmail.com","Assistance");
+                
+                var fromAdress = new MailAddress("hbnkii2@gmail.com", "Client");
+                var subject = form.Subject;
+                var content = form.Message;
+                var smtp = new SmtpClient
+                {
+                    Host = "smtp.gmail.com",
+                    Port = 587,
+                    EnableSsl = true,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential(fromAdress.Address, fromPassword)
+                };
+                using (var message = new MailMessage(fromAdress, toAdress)
+                {
+                    Subject = subject,
+                    Body = $"From: {form.Email}\n\n{form.Message}"
+                })
+                {
+                    message.ReplyToList.Add(new MailAddress(form.Email));
+                    smtp.Send(message);
+                }
+
+                return Ok(new { message = "Email sent successfully" });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
